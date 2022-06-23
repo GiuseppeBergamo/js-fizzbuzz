de2558238bdc187c7a0b28bea7ef282dc98c872b
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-06-23 17:34:48 +0200

    fine esercizio base

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ console.log("JS OK");
 
 1- scrivere il ciclo da 1 a 100
 2- trovare i multipli di 3 e di 5
-3- sostituirle i multipli con "fizz", "buzz" e "fizzbuzz"
+3- sostituire i multipli con "fizz", "buzz" e "fizzbuzz"
 4-
 
 */
@@ -14,6 +14,12 @@ console.log("JS OK");
 
 
 
-for (let i = 1; i <= 100; (i++)) {
-    console.log(i);
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("fizzbuzz");
+    } else if (i % 3 == 0) {
+        console.log("fizz")
+    } else if (i % 5 == 0) {
+        console.log("buzz")
+    } else console.log(i);
 }
