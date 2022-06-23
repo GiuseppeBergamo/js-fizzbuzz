d209528c01f0d0840053ff9471ae2253a3ef37cb
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-06-23 17:00:12 +0200

    added cicle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log("JS OK");
+
+
+/*
+
+1- scrivere il ciclo da 1 a 100
+2- trovare i multipli di 3 e di 5
+3- sostituirle i multipli con "fizz", "buzz" e "fizzbuzz"
+4-
+
+*/
+
+
+
+
+
+for (let i = 1; i <= 100; (i++)) {
+    console.log(i);
+}
